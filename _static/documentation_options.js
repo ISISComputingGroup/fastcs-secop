390dd7533f1b7f83f4f39c1874da7aebdc83a441
Tom-Willemsen <Tom-Willemsen@users.noreply.github.com>
2026-09-23 04:50:42 +0000

    deploy: 775c34d4730dad6e5d1b7a9c839798820f721063

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.2.1.dev46+g775c34d47',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
